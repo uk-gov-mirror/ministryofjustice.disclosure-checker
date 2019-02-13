5916f5a42933b611335525e563338a82cf2e48fb
Beverley Newing <bnewing92@hotmail.co.uk>
2019-02-13 14:37:47 +0000

    Adds page titles
    This adds in page title functionality to views. It also adds in some of the entries in the locales file required for the page title structure, like service name.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+  def service_name
+    t('service.name')
+  end
+
+    def title(page_title)
+        content_for :page_title, [page_title.presence, service_name, 'GOV.UK'].compact.join(' - ')
+    end
+  
+    def fallback_title
+        exception = StandardError.new("page title missing: #{controller_name}##{action_name}")
+        raise exception if Rails.application.config.consider_all_requests_local
+        # Raven.capture_exception(exception)
+
+        title ''
+    end
+
 end
