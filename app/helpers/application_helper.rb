fea0269cb1e308a73c9e5b18d297eda7be5acf9c
Beverley Newing <bnewing92@hotmail.co.uk>
2019-02-13 14:50:15 +0000

    Fix file formatting issues
    There were a few extra spaces that had been left in a few files. This commit removes them.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,16 @@
 module ApplicationHelper
-
   def service_name
     t('service.name')
   end
 
-    def title(page_title)
-        content_for :page_title, [page_title.presence, service_name, 'GOV.UK'].compact.join(' - ')
-    end
-  
-    def fallback_title
-        exception = StandardError.new("page title missing: #{controller_name}##{action_name}")
-        raise exception if Rails.application.config.consider_all_requests_local
-        # Raven.capture_exception(exception)
+  def title(page_title)
+    content_for :page_title, [page_title.presence, service_name, 'GOV.UK'].compact.join(' - ')
+  end
 
-        title ''
-    end
-
+  def fallback_title
+    exception = StandardError.new("page title missing: #{controller_name}##{action_name}")
+    raise exception if Rails.application.config.consider_all_requests_local
+    # Raven.capture_exception(exception)
+    title ''
+  end
 end
