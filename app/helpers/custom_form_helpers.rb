9f4f4902d0da264aa368b80df821048c12e412da
Jesus Laiz <zheileman@users.noreply.github.com>
2019-02-15 12:05:17 +0000

    Add custom form helpers
    To facilitate repetitive form markup or common actions, these helpers will output already pre-built markup.
    For now only the `continue` button. But we can add more in the future.

---
+++ app/helpers/custom_form_helpers.rb
@@ -0,0 +1,15 @@
+module CustomFormHelpers
+  delegate :t, to: :@template
+
+  def continue_button(continue: :continue)
+    content_tag(:div, class: 'form-submit') do
+      submit_button(continue)
+    end
+  end
+
+  private
+
+  def submit_button(i18n_key, opts = {})
+    submit t("helpers.submit.#{i18n_key}"), {class: 'button'}.merge(opts)
+  end
+end
--- config/initializers/form_builder.rb
+++ config/initializers/form_builder.rb
@@ -1,1 +1,5 @@
 ActionView::Base.default_form_builder = GovukElementsFormBuilder::FormBuilder
+
+ActionView::Base.default_form_builder.class_eval do
+  include CustomFormHelpers
+end
