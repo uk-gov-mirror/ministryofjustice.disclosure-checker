0c05ec9bc22acffeedf06b2a61eb7abcd602b13b
Jesus Laiz <zheileman@users.noreply.github.com>
2019-03-28 15:02:37 +0000

    Serve static assets with nginx, locally (docker-compose)
    This is a first step towards serving the static assets with nginx instead of Rails, and also eventually the "maintenance" page.
    
    Adapted the `docker-compose` configuration to replicate what would be a real production setup, with nginx reverse proxy, and created a basic Dockerfile.nginx to create the nginx image.
    
    Added to CircleCI configuration the build and push of the nginx docker image (but it will not have any effect yet on k8s until we also create the corresponding configuration).
    
    Removed static error pages in /public (except one that will be used by nginx) as any other kind of errors are served by Rails directly.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
-  include ErrorHandling
   include SecurityHandling
+  include ErrorHandling
+
+  # This is required to get request attributes in to the production logs.
+  # See the various lograge configurations in `production.rb`.
+  def append_info_to_payload(payload)
+    super
+    payload[:referrer] = request&.referrer
+    payload[:session_id] = request&.session&.id
+    payload[:user_agent] = request&.user_agent
+  end
 
   def current_disclosure_check
     @_current_disclosure_check ||= DisclosureCheck.find_by_id(session[:disclosure_check_id])
