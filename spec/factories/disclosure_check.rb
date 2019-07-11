94838f8ff64756563f91762c5f26c0955301b2f5
Jesus Laiz <zheileman@users.noreply.github.com>
2019-07-11 16:39:47 +0200

    Format result page dates
    This will format the user-provided dates following ISO 8601.
    
    Before: `2018-12-01`
    Now: `01/12/2018`
    
    Updated the factory to produce predictable dates to simplify and avoid flaky tests.

--- spec/factories/disclosure_check.rb
+++ spec/factories/disclosure_check.rb
@@ -1,8 +1,11 @@
+# Important: factories should always produce predictable data, specially dates.
+# This will avoid headaches with flaky tests.
+#
 FactoryBot.define do
   factory :disclosure_check do
     kind { CheckKind::CAUTION }
-    known_date { Faker::Date.backward(14).strftime('%Y-%m-%d') }
-    under_age { 'yes' }
+    known_date { Date.new(2018, 10, 31) }
+    under_age { GenericYesNo::YES }
     caution_type { CautionType::YOUTH_SIMPLE_CAUTION }
 
     trait :conviction do
@@ -22,7 +25,7 @@ FactoryBot.define do
 
     trait :youth_conditional_caution do
       caution_type { CautionType::YOUTH_CONDITIONAL_CAUTION }
-      conditional_end_date { Faker::Date.backward(8).strftime('%Y-%m-%d') }
+      conditional_end_date { Date.new(2018, 12, 25) }
     end
   end
 end
