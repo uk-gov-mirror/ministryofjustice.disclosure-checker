9d226c58571e60777d48aa1393c240b26d821119
Jesus Laiz <zheileman@users.noreply.github.com>
2019-07-31 16:23:05 +0100

    Cleanup contact page and links
    At least for now, we are not going to have an intermediate contact page and we will use the feedback link in the phase banner to get feedback.

--- spec/controllers/about_controller_spec.rb
+++ spec/controllers/about_controller_spec.rb
@@ -1,13 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe AboutController do
-  describe '#contact' do
-    it 'renders the expected page' do
-      get :contact
-      expect(response).to render_template(:contact)
-    end
-  end
-
   describe '#cookies' do
     it 'renders the expected page' do
       get :cookies
