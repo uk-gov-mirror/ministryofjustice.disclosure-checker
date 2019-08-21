8301915b7b6860ad64a4cd3ad010cb870f7dfd0a
Jesus Laiz <zheileman@users.noreply.github.com>
2019-08-21 16:14:16 +0100

    Add a few i18n sanity check tests
    It was easy to change some copy in one place and miss another place.
    
    With these simple tests we will have at least an early warning to ensure we don't forget to update the locales in all the right places.

--- spec/i18n_spec.rb
+++ spec/i18n_spec.rb
@@ -9,4 +9,34 @@ RSpec.describe 'I18n' do
     expect(missing_keys).to be_empty,
                             "Missing #{missing_keys.leaves.count} i18n keys, run `i18n-tasks missing' to show them"
   end
+
+  # Note: The following sanity checks will ensure we have the same keys and values in both places,
+  # so we don't inadvertently update just once place and not the other.
+  # It doesn't matter the order of the keys in the locales, what matters is the content.
+  #
+  context 'shared dictionaries sanity checks' do
+    it 'caution types in `helpers.yml` matches caution types in `results.yml`' do
+      expect(
+        i18n.tree('en.helpers.label.steps_caution_caution_type_form.caution_type').to_hash
+      ).to eq(
+        i18n.tree('en.results/caution.caution_type.answers').to_hash
+      )
+    end
+
+    it 'convictions types in `helpers.yml` matches convictions types in `results.yml`' do
+      expect(
+        i18n.tree('en.helpers.label.steps_conviction_conviction_type_form.conviction_type').to_hash
+      ).to eq(
+        i18n.tree('en.results/conviction.conviction_type.answers').to_hash
+      )
+    end
+
+    it 'convictions subtypes in `helpers.yml` matches convictions subtypes in `results.yml`' do
+      expect(
+        i18n.tree('en.helpers.label.steps_conviction_conviction_subtype_form.conviction_subtype').to_hash
+      ).to eq(
+        i18n.tree('en.results/conviction.conviction_subtype.answers').to_hash
+      )
+    end
+  end
 end
