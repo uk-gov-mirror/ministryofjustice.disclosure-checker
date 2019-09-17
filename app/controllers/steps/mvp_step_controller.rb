43fe23cb98e79072d903c238401cc589dad26b51
Jesus Laiz <zheileman@users.noreply.github.com>
2019-09-17 09:24:06 +0100

    Fix some styling issues
    And remove the now unused `Steps::MvpStepController`.

--- app/controllers/steps/mvp_step_controller.rb
+++
@@ -1,7 +0,0 @@
-module Steps
-  class MvpStepController < StepController
-    skip_before_action :check_http_credentials,
-                       :check_disclosure_check_presence,
-                       :check_disclosure_check_not_completed
-  end
-end
