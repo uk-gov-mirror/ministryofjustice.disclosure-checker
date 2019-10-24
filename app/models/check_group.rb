5b8194d231a9ba5f344c219ec307b57a16fff570
Tony Mckernan <anthony.mckernan@digital.justice.gov.uk>
2019-10-24 09:52:28 +0100

    Add completed disclosure check scope  in check group model (#280)

--- app/models/check_group.rb
+++ app/models/check_group.rb
@@ -1,4 +1,6 @@
 class CheckGroup < ApplicationRecord
   belongs_to :disclosure_report, default: -> { create_disclosure_report }
   has_many :disclosure_checks, dependent: :destroy
+
+  scope :with_completed_checks, -> { joins(:disclosure_checks).where(disclosure_checks: { status: :completed }) }
 end
