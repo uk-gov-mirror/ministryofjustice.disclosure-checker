a803c9ca4df15b8ed80b59b48d296b887f5cf258
Jesus Laiz <zheileman@users.noreply.github.com>
2019-12-10 11:09:06 +0000

    Feature-flag the approximate date check boxes
    We don't want this showing on production just yet, but we want it on staging/local.
    
    Same as we did with bail, we are going to feature-flag it based on the environment.

--- app/helpers/experiments_helper.rb
+++ app/helpers/experiments_helper.rb
@@ -1,4 +1,9 @@
 module ExperimentsHelper
+  # Approximate date check boxes only active on local/staging envs
+  def approximate_dates_enabled?
+    Rails.env.development? || ENV.key?('DEV_TOOLS_ENABLED')
+  end
+
   # Bail journey only active on local/staging envs
   def bail_enabled?
     Rails.env.development? || ENV.key?('DEV_TOOLS_ENABLED')
