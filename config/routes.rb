c5b6d3d2715bff6728b6f05e3e543f5442155632
David Silva <david.dasilva@digital.justice.gov.uk>
2020-03-30 15:09:00 +0100

    add page for compensation over £100

--- config/routes.rb
+++ config/routes.rb
@@ -59,6 +59,7 @@ Rails.application.routes.draw do
       edit_step :conviction_bail
       edit_step :conviction_bail_days
       edit_step :compensation_paid
+      edit_step :compensation_paid_amount
       edit_step :compensation_payment_date
       show_step :compensation_not_paid
       edit_step :motoring_endorsement
