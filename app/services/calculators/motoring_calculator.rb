276c293288241960e109a23bf6a0b05d4160df77
David Silva <Davidslv@users.noreply.github.com>
2020-05-22 13:52:03 +0100

    Calculators::Motoring::Adult::Fine

--- app/services/calculators/motoring_calculator.rb
+++ app/services/calculators/motoring_calculator.rb
@@ -3,26 +3,6 @@ module Calculators
     ENDORSEMENT_THRESHOLD = 60
     YOUTH_ENDORSEMENT_THRESHOLD = 30
 
-    # If an endorsement was received
-    # start_date + 5 years
-    # If an endorsement was not received
-    # start_date + 1 year
-    # If under age start_date + 6 months
-    class MotoringFine < MotoringCalculator
-      REHABILITATION_1 = { months: 60 }.freeze
-      REHABILITATION_2 = { months: 12 }.freeze
-      UNDER_AGE_REHABILITATION_1 = { months: 30 }.freeze
-      UNDER_AGE_REHABILITATION_2 = { months: 6 }.freeze
-
-      def expiry_date
-        return conviction_start_date.advance(self.class::REHABILITATION_1) if motoring_endorsement? && !under_age?
-        return conviction_start_date.advance(self.class::UNDER_AGE_REHABILITATION_1) if motoring_endorsement? && under_age?
-        return conviction_start_date.advance(self.class::UNDER_AGE_REHABILITATION_2) if under_age?
-
-        conviction_start_date.advance(self.class::REHABILITATION_2)
-      end
-    end
-
     # If an endorsement was received:
     # start_date + 5 years
     # If an endorsement was not received:
--- app/value_objects/conviction_type.rb
+++ app/value_objects/conviction_type.rb
@@ -97,7 +97,7 @@ class ConvictionType < ValueObject
     ADULT_SERVICE_DETENTION             = new(:adult_service_detention,            parent: ADULT_MILITARY, skip_length: true, calculator_class: Calculators::AdditionCalculator::StartPlusTwelveMonths),
 
     ADULT_DISQUALIFICATION              = new(:adult_disqualification,             parent: ADULT_MOTORING, calculator_class: Calculators::Motoring::Adult::Disqualification),
-    ADULT_MOTORING_FINE                 = new(:adult_motoring_fine,                parent: ADULT_MOTORING, skip_length: true, calculator_class: Calculators::MotoringCalculator::MotoringFine),
+    ADULT_MOTORING_FINE                 = new(:adult_motoring_fine,                parent: ADULT_MOTORING, skip_length: true, calculator_class: Calculators::Motoring::Adult::Fine),
     ADULT_PENALTY_NOTICE                = new(:adult_penalty_notice,               parent: ADULT_MOTORING, skip_length: true, calculator_class: Calculators::Motoring::Adult::PenaltyNotice),
     ADULT_PENALTY_POINTS                = new(:adult_penalty_points,               parent: ADULT_MOTORING, skip_length: true, calculator_class: Calculators::MotoringCalculator::PenaltyPoints),
 
