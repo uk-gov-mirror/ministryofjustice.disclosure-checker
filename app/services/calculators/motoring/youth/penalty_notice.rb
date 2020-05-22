70c87611626ccacdaaa5076b2763e9542b866b23
David Silva <Davidslv@users.noreply.github.com>
2020-05-22 12:11:55 +0100

    Calculators::Motoring::Youth::PenaltyNotice

---
+++ app/services/calculators/motoring/youth/penalty_notice.rb
@@ -0,0 +1,20 @@
+module Calculators
+  module Motoring
+    module Youth
+      # If an endorsement was received
+      # start_date + 2.5 years
+
+      # If an endorsement was not received
+      # Go to different result page: https://moj-disclosure-checker.herokuapp.com/motoring/v3/fpn-no-conviction
+      class PenaltyNotice < MotoringCalculator
+        REHABILITATION_1 = { months: 30 }.freeze
+
+        def expiry_date
+          return conviction_start_date.advance(REHABILITATION_1) if motoring_endorsement?
+
+          :no_record
+        end
+      end
+    end
+  end
+end
