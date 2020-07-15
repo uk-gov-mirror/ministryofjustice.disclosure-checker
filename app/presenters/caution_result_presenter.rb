16f37cb4457c7d4575a6f519dc6164a4db5ab354
David Silva <Davidslv@users.noreply.github.com>
2020-07-15 16:59:27 +0100

    Update adults simple conviction feature
    Co-Authored-By: Natalie Seeto <natalie.seeto@digital.justice.gov.uk>

--- app/presenters/caution_result_presenter.rb
+++ app/presenters/caution_result_presenter.rb
@@ -6,11 +6,7 @@ class CautionResultPresenter < ResultsPresenter
   def variant
     return 'caution_simple' if caution_type.simple?
 
-    tense = if expiry_date.instance_of?(Date)
-              expiry_date.past? ? :spent : :not_spent
-            else
-              expiry_date
-            end
+    tense = expiry_date.past? ? :spent : :not_spent
 
     # The tense can be one of these values: spent, not_spent, never_spent or no_record
     [disclosure_check.kind, tense].join('_')
