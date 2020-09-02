2a25bf7e73373770fd0edbb5b1cb35f23dd5aab9
Jesus Laiz <zheileman@users.noreply.github.com>
2020-09-02 15:41:49 +0100

    Migrate `conviction_known_date` to use new gem
    Similar to previous PR to migrate gems, but this one is a bit special as this is the first form to be migrated with our custom `i18n_attribute` that we created to allow a same form to be reused in multiple views with different legends and hint texts.
    
    This is a preliminary version of a potential solution that more or less maintain the same interface and same locales and should be easy to migrate the rest of the forms.

--- app/helpers/custom_form_helpers.rb
+++ app/helpers/custom_form_helpers.rb
@@ -3,9 +3,28 @@ module CustomFormHelpers
     submit_button(continue)
   end
 
+  # Used to customise legends when reusing the same view
+  def i18n_legend
+    I18n.t(
+      object.i18n_attribute, scope: scope_for_locale(:legend), default: :default
+    )
+  end
+
+  # Used to customise hints when reusing the same view
+  # Hints support markup so ensure locale keys ends with `_html`
+  def i18n_hint
+    I18n.t(
+      "#{object.i18n_attribute}_html", scope: scope_for_locale(:hint), default: :default_html
+    ).html_safe
+  end
+
   private
 
   def submit_button(i18n_key, opts = {}, &block)
     govuk_submit I18n.t("helpers.buttons.#{i18n_key}"), opts, &block
+  end
+
+  def scope_for_locale(context)
+    [:helpers, context, object_name]
   end
 end
--- spec/forms/steps/conviction/known_date_form_spec.rb
+++ spec/forms/steps/conviction/known_date_form_spec.rb
@@ -1,5 +1,33 @@
 require 'spec_helper'
 
 RSpec.describe Steps::Conviction::KnownDateForm do
-  it_behaves_like 'a date question form', attribute_name: :known_date
+  it_behaves_like 'a date question form', attribute_name: :known_date do
+    # TODO: move this context to the shared examples once all dates are migrated
+    context 'casting the date from multi parameters' do
+      context 'when date is valid' do
+        let(:date_value) { [nil, 2008, 11, 22] }
+        it { expect(subject).to be_valid }
+      end
+
+      context 'when date is not valid' do
+        let(:date_value) { [nil, 18, 11, 22] } # 2-digits year (18)
+        it { expect(subject).not_to be_valid }
+      end
+
+      context 'when a part is missing (nil or zero)' do
+        let(:date_value) { [nil, 2008, 0, 22] }
+        it { expect(subject).not_to be_valid }
+      end
+    end
+  end
+
+  describe '#i18n_attribute' do
+    before do
+      allow(subject).to receive(:conviction_subtype).and_return(:foobar)
+    end
+
+    it 'returns the key that will be used to translate legends and hints' do
+      expect(subject.i18n_attribute).to eq(:foobar)
+    end
+  end
 end
