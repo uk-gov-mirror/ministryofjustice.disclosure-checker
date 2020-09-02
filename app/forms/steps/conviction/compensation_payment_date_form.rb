fea62740fb2f8610c5cbc6e2563bfd622f783a90
Jesus Laiz <zheileman@users.noreply.github.com>
2020-09-02 11:11:55 +0100

    Migrate `compensation_payment_date` to use new gem
    Ticket: https://mojdigital.teamwork.com/#/tasks/21707830
    
    Continuation of PR #375, migrating all the dates in the service to use the new gem.

--- app/forms/steps/conviction/compensation_payment_date_form.rb
+++ app/forms/steps/conviction/compensation_payment_date_form.rb
@@ -1,12 +1,8 @@
 module Steps
   module Conviction
     class CompensationPaymentDateForm < BaseForm
-      include GovUkDateFields::ActsAsGovUkDate
-
-      attribute :compensation_payment_date, Date
+      attribute :compensation_payment_date, MultiParamDate
       attribute :approximate_compensation_payment_date, Boolean
-
-      acts_as_gov_uk_date :compensation_payment_date, error_clash_behaviour: :omit_gov_uk_date_field_error
 
       validates_presence_of :compensation_payment_date
       validates :compensation_payment_date, sensible_date: true
