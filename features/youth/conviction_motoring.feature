3a2f5cb1e1036d37752b99ecf7d686fad3a8dad5
Jesus Laiz <zheileman@users.noreply.github.com>
2021-01-21 12:11:40 +0000

    Update disqualification calculator
    Make the calculator work with the length instead of the end date, and also support `no length given` and `until further order` options.
    
    Update and extend the feature tests.

--- features/youth/conviction_motoring.feature
+++ features/youth/conviction_motoring.feature
@@ -4,29 +4,59 @@ Feature: Youth Conviction
     Then I should see "What was your motoring conviction?"
 
   @happy_path @date_travel
-  Scenario Outline: Motoring convictions
+  Scenario Outline: Motoring disqualification with length
     Given The current date is 03-07-2020
     When I choose "<subtype>"
 
     Then I should see "Did you get an endorsement?"
-    And I choose "Yes"
+    And I choose "<endorsement>"
 
     Then I should see "<known_date_header>"
     And I enter the following date 01-01-2020
 
-    Then I should see "<motoring_disqualification_end_date_header>"
-    And I enter the following date <disqualification_date>
+    Then I should see "<length_type_header>"
+    And  I choose "Months"
+
+    Then I should see "<length_header>"
+    And I fill in "Number of months" with "<length_months>"
+    And I click the "Continue" button
 
     Then I should be on "<result>"
     And I should see "<spent_date>"
 
     Examples:
-      | subtype           | known_date_header          | motoring_disqualification_end_date_header | disqualification_date | result               | spent_date                                   |
-      | Disqualification  | When did the ban start?    | When did your disqualification end?       | 01-06-2020            | /steps/check/results | This conviction will be spent on 1 July 2022 |
-      | Disqualification  | When did the ban start?    | When did your disqualification end?       | 22-05-2023            | /steps/check/results | This conviction will be spent on 22 May 2023 |
+      | subtype          | endorsement | known_date_header       | length_type_header                                                      | length_header                                | length_months | result               | spent_date                                   |
+      | Disqualification | Yes         | When did the ban start? | Was the length of the disqualification given in weeks, months or years? | What was the length of the disqualification? | 6             | /steps/check/results | This conviction will be spent on 1 July 2022 |
+      | Disqualification | No          | When did the ban start? | Was the length of the disqualification given in weeks, months or years? | What was the length of the disqualification? | 6             | /steps/check/results | This conviction was spent on 1 July 2020     |
+      | Disqualification | Yes         | When did the ban start? | Was the length of the disqualification given in weeks, months or years? | What was the length of the disqualification? | 40            | /steps/check/results | This conviction will be spent on 1 May 2023  |
+      | Disqualification | No          | When did the ban start? | Was the length of the disqualification given in weeks, months or years? | What was the length of the disqualification? | 40            | /steps/check/results | This conviction will be spent on 1 May 2023  |
 
   @happy_path @date_travel
-  Scenario Outline: Motoring convictions without length
+  Scenario Outline: Motoring disqualification without length or indefinite
+    Given The current date is 03-07-2020
+    When I choose "<subtype>"
+
+    Then I should see "Did you get an endorsement?"
+    And I choose "<endorsement>"
+
+    Then I should see "<known_date_header>"
+    And I enter the following date 01-01-2020
+
+    Then I should see "<length_type_header>"
+    And  I choose "<length_option>"
+
+    Then I should be on "<result>"
+    And I should see "<spent_date>"
+
+    Examples:
+      | subtype          | endorsement | known_date_header       | length_type_header                                                      | length_option       | result               | spent_date                                                                         |
+      | Disqualification | Yes         | When did the ban start? | Was the length of the disqualification given in weeks, months or years? | No length was given | /steps/check/results | This conviction will be spent on 1 July 2022                                       |
+      | Disqualification | No          | When did the ban start? | Was the length of the disqualification given in weeks, months or years? | No length was given | /steps/check/results | This conviction will be spent on 1 January 2022                                    |
+      | Disqualification | Yes         | When did the ban start? | Was the length of the disqualification given in weeks, months or years? | Until further order | /steps/check/results | This conviction will stay in place until another order is made to change or end it |
+      | Disqualification | No          | When did the ban start? | Was the length of the disqualification given in weeks, months or years? | Until further order | /steps/check/results | This conviction will stay in place until another order is made to change or end it |
+
+  @happy_path @date_travel
+  Scenario Outline: Motoring fine
     Given The current date is 03-07-2020
     When I choose "<subtype>"
 
@@ -45,7 +75,7 @@ Feature: Youth Conviction
       | Fine                       | No          | When were you given the fine?            | /steps/check/results | This conviction was spent on 1 July 2020        |
 
   @happy_path @date_travel
-  Scenario Outline: Motoring convictions without length (penalty points)
+  Scenario Outline: Motoring penalty points
     Given The current date is 03-07-2020
     When I choose "<subtype>"
 
--- spec/services/calculators/disqualification_calculator_spec.rb
+++ spec/services/calculators/disqualification_calculator_spec.rb
@@ -5,39 +5,49 @@ RSpec.describe Calculators::DisqualificationCalculator do
 
   let(:disclosure_check) { build(:disclosure_check,
                                  under_age: under_age,
-                                 known_date: known_date,
                                  motoring_endorsement: motoring_endorsement,
-                                 motoring_disqualification_end_date: motoring_disqualification_end_date) }
+                                 known_date: known_date,
+                                 conviction_length: conviction_length,
+                                 conviction_length_type: conviction_length_type) }
 
-  let(:known_date) { Date.new(2018, 10, 31) }
   let(:motoring_endorsement) { GenericYesNo::NO }
-  let(:motoring_disqualification_end_date) { Date.new(2020, 10, 31) }
+  let(:known_date) { Date.new(2018, 10, 31) }
+  let(:conviction_length) { nil }
+  let(:conviction_length_type) { nil }
 
   describe Calculators::DisqualificationCalculator::Youths do
     let(:under_age) { GenericYesNo::YES }
 
     context '#expiry_date' do
-      context 'with a motoring_disqualification_end_date' do
+      context 'with a length' do
         context 'with a motoring endorsement' do
           let(:motoring_endorsement) { GenericYesNo::YES }
 
           context 'less than or equal to 2.5 years' do
+            let(:conviction_length) { 2 }
+            let(:conviction_length_type) { 'years' }
+
             it { expect(subject.expiry_date.to_s).to eq((known_date + 30.months).to_s) }
           end
 
           context 'greater than 2.5 years' do
-            let(:motoring_disqualification_end_date) { Date.new(2025, 10, 31) }
-            it { expect(subject.expiry_date.to_s).to eq(motoring_disqualification_end_date.to_s) }
+            let(:conviction_length) { 3 }
+            let(:conviction_length_type) { 'years' }
+
+            it { expect(subject.expiry_date.to_s).to eq((known_date + 36.months).to_s) }
           end
         end
 
         context 'without a motoring endorsement' do
-          it { expect(subject.expiry_date.to_s).to eq(motoring_disqualification_end_date.to_s) }
+          let(:conviction_length) { 3 }
+          let(:conviction_length_type) { 'years' }
+
+          it { expect(subject.expiry_date.to_s).to eq((known_date + 36.months).to_s) }
         end
       end
 
-      context 'without a motoring_disqualification_end_date' do
-        let(:motoring_disqualification_end_date) { nil }
+      context 'without a length' do
+        let(:conviction_length) { 'no_length' }
 
         context 'with a motoring endorsement' do
           let(:motoring_endorsement) { GenericYesNo::YES }
@@ -48,6 +58,11 @@ RSpec.describe Calculators::DisqualificationCalculator do
           it { expect(subject.expiry_date.to_s).to eq('2020-10-31') }
         end
       end
+
+      context 'with an indefinite length' do
+        let(:conviction_length_type) { 'indefinite' }
+        it { expect(subject.expiry_date).to eq(ResultsVariant::INDEFINITE) }
+      end
     end
   end
 
@@ -55,35 +70,49 @@ RSpec.describe Calculators::DisqualificationCalculator do
     let(:under_age) { GenericYesNo::NO }
 
     context '#expiry_date' do
-      context 'with a motoring_disqualification_end_date' do
-        context 'with a motoring endorsement ' do
+      context 'with a length' do
+        context 'with a motoring endorsement' do
           let(:motoring_endorsement) { GenericYesNo::YES }
 
-          context 'less than or equal 5 years' do
-            it { expect(subject.expiry_date.to_s).to eq('2023-10-31') }
+          context 'less than or equal to 5 years' do
+            let(:conviction_length) { 4 }
+            let(:conviction_length_type) { 'years' }
+
+            it { expect(subject.expiry_date.to_s).to eq((known_date + 60.months).to_s) }
           end
 
           context 'greater than 5 years' do
-            let(:motoring_disqualification_end_date) { Date.new(2025, 10, 31) }
-            it { expect(subject.expiry_date.to_s).to eq(motoring_disqualification_end_date.to_s) }
+            let(:conviction_length) { 6 }
+            let(:conviction_length_type) { 'years' }
+
+            it { expect(subject.expiry_date.to_s).to eq((known_date + 72.months).to_s) }
           end
         end
 
-        context 'without a motoring endorsement ' do
-          it { expect(subject.expiry_date.to_s).to eq(motoring_disqualification_end_date.to_s) }
+        context 'without a motoring endorsement' do
+          let(:conviction_length) { 3 }
+          let(:conviction_length_type) { 'years' }
+
+          it { expect(subject.expiry_date.to_s).to eq((known_date + 36.months).to_s) }
         end
       end
 
-      context 'without a motoring_disqualification_end_date' do
-        let(:motoring_disqualification_end_date) { nil }
-        context 'with a motoring endorsement ' do
+      context 'without a length' do
+        let(:conviction_length) { 'no_length' }
+
+        context 'with a motoring endorsement' do
           let(:motoring_endorsement) { GenericYesNo::YES }
           it { expect(subject.expiry_date.to_s).to eq('2023-10-31') }
         end
 
-        context 'without a motoring endorsement ' do
+        context 'without a motoring endorsement' do
           it { expect(subject.expiry_date.to_s).to eq('2020-10-31') }
         end
+      end
+
+      context 'with an indefinite length' do
+        let(:conviction_length_type) { 'indefinite' }
+        it { expect(subject.expiry_date).to eq(ResultsVariant::INDEFINITE) }
       end
     end
   end
