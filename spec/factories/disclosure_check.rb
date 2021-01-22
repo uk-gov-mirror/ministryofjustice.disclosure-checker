c4d19e1548924d2ae23a81212871e13efe40d8f5
David Silva <Davidslv@users.noreply.github.com>
2021-01-22 14:50:56 +0000

    Add new conviction factories to disclosure check
    This commit introduces the following factory traits:
    
    - Fine
    - Motoring Disqualification
    - Motoring Penalty Points
    - Motoring Fine
    
    Suggested usage design implementations
    
    ```
    FactoryBot.create(:disclosure_check, :conviction, :youth, :with_motoring_fine)
    FactoryBot.create(:disclosure_check, :conviction, :adult, :with_motoring_fine)
    
    FactoryBot.create(:disclosure_check, :caution, :youth)
    FactoryBot.create(:disclosure_check, :caution, :adult)
    ```
    
    I've also removes under_age setting from the conviction trait, this was interfering with the order of factory setup and I didn't want that.

--- spec/factories/disclosure_check.rb
+++ spec/factories/disclosure_check.rb
@@ -9,13 +9,29 @@ FactoryBot.define do
     under_age { GenericYesNo::YES }
     caution_type { CautionType::YOUTH_SIMPLE_CAUTION }
 
+    trait :youth do
+      under_age { GenericYesNo::YES }
+    end
+
+    trait :adult do
+      under_age { GenericYesNo::NO }
+    end
+
     trait :caution
 
     trait :conviction do
       kind { CheckKind::CONVICTION }
       known_date { nil }
-      under_age { nil }
       caution_type { nil }
+    end
+
+    trait :with_known_date do
+      known_date { Date.new(2018, 10, 31) }
+    end
+
+    trait :conviction_with_known_date do
+      conviction
+      with_known_date
     end
 
     trait :dto_conviction do
@@ -50,6 +66,34 @@ FactoryBot.define do
       conviction_subtype { ConvictionType::ADULT_SUSPENDED_PRISON_SENTENCE }
       conviction_length_type { ConvictionLengthType::MONTHS }
       conviction_length { 15 }
+    end
+
+    # Financial
+
+    trait :with_fine do
+      conviction_with_known_date
+      conviction_type { self.under_age.inquiry.yes? ? ConvictionType::FINANCIAL : ConvictionType::ADULT_FINANCIAL }
+      conviction_subtype { self.under_age.inquiry.yes? ? ConvictionType::FINE : ConvictionType::ADULT_FINE }
+    end
+
+    # Motoring
+
+    trait :with_motoring_disqualification do
+      conviction_with_known_date
+      conviction_type { self.under_age.inquiry.yes? ? ConvictionType::YOUTH_MOTORING : ConvictionType::ADULT_MOTORING }
+      conviction_subtype { self.under_age.inquiry.yes? ? ConvictionType::YOUTH_DISQUALIFICATION : ConvictionType::ADULT_DISQUALIFICATION }
+    end
+
+    trait :with_motoring_penalty_points do
+      conviction_with_known_date
+      conviction_type { self.under_age.inquiry.yes? ? ConvictionType::YOUTH_MOTORING : ConvictionType::ADULT_MOTORING }
+      conviction_subtype { self.under_age.inquiry.yes? ? ConvictionType::YOUTH_PENALTY_POINTS : ConvictionType::ADULT_PENALTY_POINTS }
+    end
+
+    trait :with_motoring_fine do
+      conviction_with_known_date
+      conviction_type { self.under_age.inquiry.yes? ? ConvictionType::YOUTH_MOTORING : ConvictionType::ADULT_MOTORING }
+      conviction_subtype { self.under_age.inquiry.yes? ? ConvictionType::YOUTH_MOTORING_FINE : ConvictionType::ADULT_MOTORING_FINE }
     end
 
     trait :completed do
