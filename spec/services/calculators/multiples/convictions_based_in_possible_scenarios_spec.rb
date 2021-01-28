21e6e73ed90642ee8f46e6e1f8392da9601f97d9
David Silva <Davidslv@users.noreply.github.com>
2021-01-28 11:00:16 +0000

    Trying the new refactor on calculator

--- spec/services/calculators/multiples/convictions_based_in_possible_scenarios_spec.rb
+++ spec/services/calculators/multiples/convictions_based_in_possible_scenarios_spec.rb
@@ -7,24 +7,22 @@ RSpec.describe Calculators::Multiples::MultipleOffensesCalculator do
   let(:first_group) { disclosure_report.check_groups.build }
   let(:second_group) { disclosure_report.check_groups.build }
 
-  def save_report_and_process_calculation
+  def save_report
     disclosure_report.save
-    subject.process!
   end
 
   context 'adult' do
     context 'when same proceedings' do
       context 'when a relevant order is the longest sentence' do
         before do
-          first_group.disclosure_checks << FactoryBot.create(:disclosure_check, :adult, :with_discharge_order, :completed, known_date: Date.new(2000, 1, 1), conviction_length: 36)
-          first_group.disclosure_checks << FactoryBot.create(:disclosure_check, :adult, :with_motoring_disqualification, :completed, known_date: Date.new(2000, 1, 1))
-
-          save_report_and_process_calculation
+          first_group.disclosure_checks << build(:disclosure_check, :adult, :with_discharge_order, :completed, known_date: Date.new(2000, 1, 1), conviction_length: 36)
+          first_group.disclosure_checks << build(:disclosure_check, :adult, :with_motoring_disqualification, :completed, known_date: Date.new(2000, 1, 1))
+          save_report
         end
 
         it 'ignores the relevant order' do
           pending
-          expect(subject.spent_date_for(first_group)).to eq(Date.new(2000, 7, 1))
+          expect(subject.spent_date_for(subject.results[first_group.id])).to eq(Date.new(2000, 7, 1))
         end
       end
     end
@@ -32,29 +30,34 @@ RSpec.describe Calculators::Multiples::MultipleOffensesCalculator do
     context 'with separate proceedings' do
       context 'two convictions that overlap in time' do
         before do
-          first_group.disclosure_checks << FactoryBot.create(:disclosure_check, :adult, :with_discharge_order, :completed, known_date: Date.new(2000, 12, 2))
-          second_group.disclosure_checks << FactoryBot.create(:disclosure_check, :adult, :with_fine, :completed, known_date: Date.new(2001, 8, 6))
+          first_group.disclosure_checks << build(:disclosure_check, :adult, :with_discharge_order, :completed, known_date: Date.new(2000, 12, 2))
+          second_group.disclosure_checks << build(:disclosure_check, :adult, :with_fine, :completed, known_date: Date.new(2001, 8, 6))
 
-          save_report_and_process_calculation
+          save_report
+        end
+
+        it 'has separate proceedings' do
+          expect(subject.results[first_group.id]).to be_kind_of(Calculators::Multiples::SeparateProceedings)
+          expect(subject.results[second_group.id]).to be_kind_of(Calculators::Multiples::SeparateProceedings)
         end
 
         it 'calculates the spent date of the latest date for both convictions' do
-          expect(subject.spent_date_for(first_group)).to eq(Date.new(2002, 8, 6))
-          expect(subject.spent_date_for(second_group)).to eq(Date.new(2002, 8, 6))
+          expect(subject.spent_date_for(subject.results[first_group.id])).to eq(Date.new(2002, 8, 6))
+          expect(subject.spent_date_for(subject.results[second_group.id])).to eq(Date.new(2002, 8, 6))
         end
       end
 
       context 'two convictions that do not overlap in time' do
         before do
-          first_group.disclosure_checks << FactoryBot.create(:disclosure_check, :adult, :with_fine, :completed, known_date: Date.new(2000, 1, 1))
-          second_group.disclosure_checks << FactoryBot.create(:disclosure_check, :adult, :with_fine, :completed, known_date: Date.new(2005, 5, 5))
+          first_group.disclosure_checks << build(:disclosure_check, :adult, :with_fine, :completed, known_date: Date.new(2000, 1, 1))
+          second_group.disclosure_checks << build(:disclosure_check, :adult, :with_fine, :completed, known_date: Date.new(2005, 5, 5))
 
-          save_report_and_process_calculation
+          save_report
         end
 
         it 'calculates the spent date of each conviction separately' do
-          expect(subject.spent_date_for(first_group)).to eq(Date.new(2001, 1, 1))
-          expect(subject.spent_date_for(second_group)).to eq(Date.new(2006, 5, 5))
+          expect(subject.spent_date_for(subject.results[first_group.id])).to eq(Date.new(2001, 1, 1))
+          expect(subject.spent_date_for(subject.results[second_group.id])).to eq(Date.new(2006, 5, 5))
         end
       end
 
@@ -63,25 +66,25 @@ RSpec.describe Calculators::Multiples::MultipleOffensesCalculator do
         let(:fourth_group) { disclosure_report.check_groups.build }
 
         before do
-          first_group.disclosure_checks << FactoryBot.create(:disclosure_check, :adult, :with_fine, :completed, known_date: Date.new(2000, 12, 2))
-          second_group.disclosure_checks << FactoryBot.create(:disclosure_check, :adult, :with_fine, :completed, known_date: Date.new(2001, 2, 2))
+          first_group.disclosure_checks << build(:disclosure_check, :adult, :with_fine, :completed, known_date: Date.new(2000, 12, 2))
+          second_group.disclosure_checks << build(:disclosure_check, :adult, :with_fine, :completed, known_date: Date.new(2001, 2, 2))
 
-          third_group.disclosure_checks << FactoryBot.create(:disclosure_check, :adult, :with_prison_sentence, :completed, known_date: Date.new(2005, 1, 1), conviction_length: 60)
-          fourth_group.disclosure_checks << FactoryBot.create(:disclosure_check, :adult, :with_sexual_harm_order, :completed, known_date: Date.new(2005, 4, 2))
+          third_group.disclosure_checks << build(:disclosure_check, :adult, :with_prison_sentence, :completed, known_date: Date.new(2005, 1, 1), conviction_length: 60)
+          fourth_group.disclosure_checks << build(:disclosure_check, :adult, :with_sexual_harm_order, :completed, known_date: Date.new(2005, 4, 2))
 
-          save_report_and_process_calculation
+          save_report
         end
 
         it 'calculates the first 2 convictions as the same spent date' do
-          expect(subject.spent_date_for(first_group)).not_to eq(ResultsVariant::NEVER_SPENT)
+          expect(subject.spent_date_for(subject.results[first_group.id])).not_to eq(ResultsVariant::NEVER_SPENT)
 
-          expect(subject.spent_date_for(first_group)).to eq(subject.spent_date_for(second_group))
-          expect(subject.spent_date_for(first_group)).to eq(Date.new(2002, 2, 2))
+          expect(subject.spent_date_for(subject.results[first_group.id])).to eq(subject.spent_date_for(subject.results[second_group.id]))
+          expect(subject.spent_date_for(subject.results[first_group.id])).to eq(Date.new(2002, 2, 2))
         end
 
         it 'calculates the second 2 convictions as never spent' do
-          expect(subject.spent_date_for(third_group)).to eq(subject.spent_date_for(fourth_group))
-          expect(subject.spent_date_for(first_group)).to eq(ResultsVariant::NEVER_SPENT)
+          expect(subject.spent_date_for(third_group)).to eq(subject.spent_date_for(subject.results[fourth_group.id]))
+          expect(subject.spent_date_for(subject.results[first_group.id])).to eq(ResultsVariant::NEVER_SPENT)
         end
       end
     end
