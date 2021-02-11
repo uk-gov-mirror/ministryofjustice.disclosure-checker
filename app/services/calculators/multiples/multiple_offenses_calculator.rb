aad18858b63e4319586d4cb2ff9f303db851423e
David Silva <Davidslv@users.noreply.github.com>
2021-02-11 10:27:30 +0000

    add concept class to remember dates
    also compares them

--- app/services/calculators/multiples/multiple_offenses_calculator.rb
+++ app/services/calculators/multiples/multiple_offenses_calculator.rb
@@ -1,5 +1,23 @@
 module Calculators
   module Multiples
+    class MemorableDate
+      def initialize
+        @dates = []
+      end
+
+      def from(spent_date, other_spent_date)
+        @dates << spent_date
+        @dates << other_spent_date
+      end
+
+      def spent_date
+        return ResultsVariant::NEVER_SPENT if @dates.include?(ResultsVariant::NEVER_SPENT)
+        return ResultsVariant::INDEFINITE if @dates.include?(ResultsVariant::INDEFINITE)
+
+        @dates.max
+      end
+    end
+
     class MultipleOffensesCalculator
       attr_reader :disclosure_report,
                   :proceedings
@@ -7,6 +25,8 @@ module Calculators
       def initialize(disclosure_report)
         @disclosure_report = disclosure_report
         @proceedings = []
+        # concept
+        @memorable_date = MemorableDate.new
 
         process!
       end
@@ -16,6 +36,8 @@ module Calculators
 
         conviction_date = proceeding.conviction_date
         spent_date = proceeding.spent_date
+
+        # TODO: remove this line when we find it's not needed
         current_spent_date = proceeding.spent_date
 
         # Cautions are always dealt with separately and do not have drag-through
@@ -34,20 +56,14 @@ module Calculators
         #   Assuming there are overlaps, everything that has an end date given before a
         #   "never spent" conviction becomes "never spent". Everything afterwards is not
         #   affected by the "never spent". Same for "indefinite".
-        #
-        # convictions_by_date.map(&:conviction_date)
-        # [conviction_B, conviction_A, conviction_C]
-        # => [Sat, 01 Jan 2000, Mon, 01 Jan 2001, Fri, 01 Mar 2002]
 
-        # convictions_by_date.map(&:spent_date_without_relevant_orders)
-        # => [Fri, 01 Feb 2002, Wed, 01 Jan 2003, Thu, 01 Jun 2006]
-
+        # TODO: remove this line
         conviction_B, conviction_A, conviction_C = convictions_by_date
 
 
         convictions_by_date.without(proceeding).each do |conviction|
           other_conviction_date = conviction.conviction_date
-          other_spent_date = conviction.new_spent_date.nil? ? conviction.spent_date_without_relevant_orders : conviction.new_spent_date
+          other_spent_date = conviction.spent_date_without_relevant_orders
 
           # solo relevant order
           # if there's only one sentence and it is a relevant order
@@ -60,25 +76,6 @@ module Calculators
           # the comparison to know if there's an overlap in conviction dates
           # should be done without the relevant order
           # because relevant orders do not dictacte the spent_date of another conviction.
-
-          # Conviction A compares with Conviction B
-          # => Wed, 01 Jan 2003 in? (Sat, 01 Jan 2000 .. Fri, 01 Feb 2002)
-          # => false
-
-          # Conviction A compares with conviction C
-          # => 2003-01-01 in ? 2002-03-01 .. 2006-06-01
-          # => true
-          # Conviction A spent date becomes Conviction C
-          # spent date: 2006, 6, 1
-          # Although we have already compared A with B
-          # meaning that Conviction B won't be affected because
-          # Conviction C does not overlap with Conviction B
-
-          puts "#{spent_date_without_relevant_order} in ? #{other_conviction_date} .. #{other_spent_date}"
-
-          # spent_date_without_relevant_order.to_date.in?( other_conviction_date..other_spent_date.to_date )
-          # binding.pry
-
           next unless spent_date_without_relevant_order.to_date.in?(
             other_conviction_date..other_spent_date.to_date
           )
@@ -96,15 +93,13 @@ module Calculators
 
           next if conviction_date > other_conviction_date
 
-          # binding.pry
-
           # If the spent date falls inside another rehabilitation, we do drag-through.
           # The `spent_date` or the `other_spent_date` can be NEVER_SPENT or INDEFINITE.
+          @memorable_date.from(spent_date, other_spent_date)
           spent_date = other_spent_date
-          proceeding.new_spent_date = spent_date
         end
 
-        spent_date
+        @memorable_date.spent_date.nil? ? spent_date : @memorable_date.spent_date
       end
 
       def all_spent?
