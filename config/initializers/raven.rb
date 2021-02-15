5bf6c61792635847e163fb85fc5d39e0c39c4370
Jesus Laiz <zheileman@users.noreply.github.com>
2021-02-15 16:36:47 +0000

    Submit to sentry missing template exceptions
    As part of ticket https://trello.com/c/HML7Aa42 we found out that Sentry by default was ignoring (not submitting) any `ActionView::MissingTemplate` errors so it was more difficult to find out where our code was failing.
    
    Remove from the list of excluded exceptions `ActionView::MissingTemplate` so we can get these, including their backtrace.

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -3,4 +3,9 @@ Raven.configure do |config|
   config.environments = %w( production )
   config.silence_ready = true
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+
+  # Ensure we get ActionView::MissingTemplate errors
+  config.excluded_exceptions -= %w(
+    ActionView::MissingTemplate
+  )
 end
