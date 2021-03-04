ec951ac49e7663e73ce2e1d21accc560c4372758
David Silva <Davidslv@users.noreply.github.com>
2021-03-04 15:33:24 +0000

    Update hint text underneat motoring page header
    Story: https://trello.com/c/jdhA6rjb/64-dc-endorsed-fpns-journey

--- spec/forms/steps/conviction/conviction_subtype_form_spec.rb
+++ spec/forms/steps/conviction/conviction_subtype_form_spec.rb
@@ -38,6 +38,74 @@ RSpec.describe Steps::Conviction::ConvictionSubtypeForm do
   describe '#save' do
     it_behaves_like 'a value object form', attribute_name: :conviction_subtype, example_value: 'referral_order'
 
+    context 'when conviction type is motoring' do
+      context 'and conviction subtype is youth_penalty_notice' do
+        let(:conviction_type) { 'youth_motoring' }
+        let(:conviction_subtype) { 'youth_penalty_notice' }
+
+        it 'saves the record with motoring endorsement saved with `yes` value' do
+          expect(disclosure_check).to receive(:update).with(
+            conviction_subtype: conviction_subtype,
+            # Dependent attributes to be reset
+            known_date: nil,
+            conviction_bail: nil,
+            conviction_bail_days: nil,
+            conviction_length: nil,
+            conviction_length_type: nil,
+            compensation_paid: nil,
+            compensation_payment_date: nil,
+            motoring_endorsement: GenericYesNo::YES,
+          ).and_return(true)
+
+          expect(subject.save).to be(true)
+        end
+
+        context 'when conviction_subtype is already the same on the model' do
+          let(:disclosure_check) {
+            instance_double(DisclosureCheck, conviction_type: conviction_type, conviction_subtype: conviction_subtype)
+          }
+
+          it 'does not save the record but returns true' do
+            expect(disclosure_check).to_not receive(:update)
+            expect(subject.save).to be(true)
+          end
+        end
+      end
+
+      context 'and conviction subtype is adult_penalty_notice' do
+        let(:conviction_type) { 'adult_motoring' }
+        let(:conviction_subtype) { 'adult_penalty_notice' }
+
+        it 'saves the record with motoring endorsement saved with `yes` value' do
+          expect(disclosure_check).to receive(:update).with(
+            conviction_subtype: conviction_subtype,
+            # Dependent attributes to be reset
+            known_date: nil,
+            conviction_bail: nil,
+            conviction_bail_days: nil,
+            conviction_length: nil,
+            conviction_length_type: nil,
+            compensation_paid: nil,
+            compensation_payment_date: nil,
+            motoring_endorsement: GenericYesNo::YES,
+          ).and_return(true)
+
+          expect(subject.save).to be(true)
+        end
+
+        context 'when conviction_subtype is already the same on the model' do
+          let(:disclosure_check) {
+            instance_double(DisclosureCheck, conviction_type: conviction_type, conviction_subtype: conviction_subtype)
+          }
+
+          it 'does not save the record but returns true' do
+            expect(disclosure_check).to_not receive(:update)
+            expect(subject.save).to be(true)
+          end
+        end
+      end
+    end
+
     context 'when form is valid' do
       let(:conviction_subtype) { 'referral_order' }
 
@@ -62,7 +130,6 @@ RSpec.describe Steps::Conviction::ConvictionSubtypeForm do
         let(:disclosure_check) {
           instance_double(DisclosureCheck, conviction_type: conviction_type, conviction_subtype: conviction_subtype)
         }
-        let(:conviction_subtype) { 'referral_order' }
 
         it 'does not save the record but returns true' do
           expect(disclosure_check).to_not receive(:update)
